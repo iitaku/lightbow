7a45b4b6430aa08e3d0c5be409725e914a57524c
Takuro Iizuka <iitaku@TiMBP.local>
2023-01-29 14:14:27 -0800

    Refactored

--- code.py
+++ code.py
@@ -7,25 +7,62 @@ import board
 from rainbowio import colorwheel
 import neopixel
 
-# CUSTOMIZE YOUR COLOR HERE:
-# (red, green, blue) -- each 0 (off) to 255 (brightest)
-# COLOR = (255, 0, 0)  # red
-# COLOR = (100, 0, 255)  # purple
-#COLOR = (0, 100, 255) #cyan
-# COLOR = (255, 0, 0) #green
-COLOR = (0, 255, 0) #green
+def mix(color_1, color_2, weight_2):
+    """
+    Blend between two colors with a given ratio.
+    @param color_1:  first color, as an (r,g,b) tuple
+    @param color_2:  second color, as an (r,g,b) tuple
+    @param weight_2: Blend weight (ratio) of second color, 0.0 to 1.0
+    @return: (r,g,b) tuple, blended color
+    """
+    if weight_2 < 0.0:
+        weight_2 = 0.0
+    elif weight_2 > 1.0:
+        weight_2 = 1.0
+    weight_1 = 1.0 - weight_2
+    return (int(color_1[0] * weight_1 + color_2[0] * weight_2),
+            int(color_1[1] * weight_1 + color_2[1] * weight_2),
+            int(color_1[2] * weight_1 + color_2[2] * weight_2))
 
 PIXEL_PIN = board.A1
 NUM_PIXELS = 30
 
-strip = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, brightness=0.3, auto_write=False)
+strip = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, brightness=1.0, auto_write=False)
 strip.fill(0)
 strip.show()
- 
-# "Idle" color is 1/4 brightness, "swinging" color is full brightness...
-COLOR_IDLE = (int(COLOR[0] / 1), int(COLOR[1] / 1), int(COLOR[2] / 1))
-COLOR_SWING = COLOR
-COLOR_HIT = (255, 255, 255)  # "hit" color is white
+
+def color_off():
+    vs = [0] * NUM_PIXELS
+    vs[0] = (255, 255, 255)
+    vs[1] = (255, 255, 255)
+    return vs
+
+def color_mono(c):
+    return [c] * NUM_PIXELS
+
+def color_gradation(c1, c2):
+    vs = []
+    for i in range(NUM_PIXELS):
+        vs.append(mix(c1, c2, math.pow(i/NUM_PIXELS, 3)))
+    return vs
+
+def color_step(cs, n):
+    vs = []
+    for i in range(0, NUM_PIXELS, n):
+        for j in range(0, n):
+            vs.append(cs[int(i/n) % len(cs)])
+    return vs
+
+mode = 0
+colors = []
+colors.append(color_off())
+# colors.append(color_mono((0, 255, 0))) # Green
+# colors.append(color_mono((255, 0, 0))) # Red
+# colors.append(color_mono((255, 255, 255))) # Silver
+# colors.append(color_mono((255, 120, 0))) # Gold
+# colors.append(color_gradation((0, 0, 255), (255, 255, 255))) # Snow 
+colors.append(color_gradation((255, 20, 50), (252, 50, 150))) # Cherry blossom 
+# colors.append(color_step(((50, 0, 0), (0, 50, 0)), 8))
 
 def power(duration, reverse):
     """
@@ -53,7 +90,8 @@ def power(duration, reverse):
             if reverse:
                 strip[threshold:prev] = [0] * -num
             else:
-                strip[prev:threshold] = [COLOR_IDLE] * num
+                for i in range(num):
+                    strip[prev+i] = colors[mode][prev+i]
             strip.show()
             # NeoPixel writes throw off time.monotonic() ever so slightly
             # because interrupts are disabled during the transfer.
@@ -62,45 +100,29 @@ def power(duration, reverse):
             start_time -= NUM_PIXELS * 0.00003
             prev = threshold
 
-    if reverse:
-        strip.fill(0)                            # At end, ensure strip is off
-    else:
-        strip.fill(COLOR_IDLE)                   # or all pixels set on
-    strip.show()
+    # At end, ensure strip is off except first two to prevent battery sleep
+    strip[0] = (255, 255, 255)
+    strip[1] = (255, 255, 255)
 
-def mix(color_1, color_2, weight_2):
-    """
-    Blend between two colors with a given ratio.
-    @param color_1:  first color, as an (r,g,b) tuple
-    @param color_2:  second color, as an (r,g,b) tuple
-    @param weight_2: Blend weight (ratio) of second color, 0.0 to 1.0
-    @return: (r,g,b) tuple, blended color
-    """
-    if weight_2 < 0.0:
-        weight_2 = 0.0
-    elif weight_2 > 1.0:
-        weight_2 = 1.0
-    weight_1 = 1.0 - weight_2
-    return (int(color_1[0] * weight_1 + color_2[0] * weight_2),
-            int(color_1[1] * weight_1 + color_2[1] * weight_2),
-            int(color_1[2] * weight_1 + color_2[2] * weight_2))
+    strip.show()
 
 switch = digitalio.DigitalInOut(board.A2)
 switch.direction = digitalio.Direction.INPUT
 switch.pull = digitalio.Pull.DOWN
 
-state = False
-
 power(1.2, False)
 
-# i = 0
+elapsed = 0
+start_time = time.monotonic()
 while True:
-
-    # strip.brightness = 0.3 + math.sin(math.radians(i)) * 0.1
-    # strip.show()
-    # i += 1
-
     if switch.value:
-        state = not state
-        power(1.2, state)
-    
+        elapsed = time.monotonic() - start_time
+    else:
+        start_time = time.monotonic()
+        # elapsed = 0
+
+    if elapsed > 1:
+        mode = (mode + 1) % len(colors)
+        power(1.2, False)
+        elapsed = 0
+        start_time = time.monotonic()
